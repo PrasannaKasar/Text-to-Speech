e063c6a5b9746f45e3002550700eee7d954dec97
PrasannaKasar <154019744+PrasannaKasar@users.noreply.github.com>
2024-10-11 02:41:34 +0530

    indentations fixed of LJDataset

--- preprocess.py
+++ preprocess.py
@@ -22,41 +22,41 @@ class LJDatasets(Dataset):
         self.audio_annotations = pd.read_csv(csv_file, sep='|', header=None)
         self.root_dir = root_dir
         
-def load_wav(self, filename):
-    return librosa.load(filename, sr=hp.sample_rate) 
-
-def __len__(self):
-    return len(self.audio_annotations)
-
-def __getitem__(self, index):
-    wav_name = os.path.join(self.root_dir, self.audio_annotations.ix[index, 0]) + '.wav'   # .ix[row, column]
-    text = self.audio_annotations.ix[index, 1]    # .ix[row, column]
+    def load_wav(self, filename):
+        return librosa.load(filename, sr=hp.sample_rate) 
     
-    text = np.asarray(text_to_sequence(text, [hp.cleaners]), dtype=np.int32)
-    melspectrogram_data = np.load(wav_name[:-4] + '.pt.npy')   # audio_01.wav -> audio_01.pt.npy
-    mel_input = np.concatenate([np.zeros([1,hp.num_mels], np.float32), melspectrogram_data[:-1,:]], axis=0)
-    """
-        Eg: 3 time steps which is no. of rows, each time step with num_mels= 3
-        mel = [
-            [1,2,3]
-            [3,4,5]
-            [6,7,8]         
-        ]
+    def __len__(self):
+        return len(self.audio_annotations)
+    
+    def __getitem__(self, index):
+        wav_name = os.path.join(self.root_dir, self.audio_annotations.ix[index, 0]) + '.wav'   # .ix[row, column]
+        text = self.audio_annotations.ix[index, 1]    # .ix[row, column]
         
-        mel_input = [
-            [0,0,0]
-            [1,2,3]
-            [3,4,5]
-        ]
+        text = np.asarray(text_to_sequence(text, [hp.cleaners]), dtype=np.int32)
+        melspectrogram_data = np.load(wav_name[:-4] + '.pt.npy')   # audio_01.wav -> audio_01.pt.npy
+        mel_input = np.concatenate([np.zeros([1,hp.num_mels], np.float32), melspectrogram_data[:-1,:]], axis=0)
+        """
+            Eg: 3 time steps which is no. of rows, each time step with num_mels= 3
+            mel = [
+                [1,2,3]
+                [3,4,5]
+                [6,7,8]         
+            ]
+            
+            mel_input = [
+                [0,0,0]
+                [1,2,3]
+                [3,4,5]
+            ]
+            
+        """
+        text_length = len(text)
+        position_of_text = np.arrange(1, text_length + 1) #position_of_text=[1,2,...,text_length(int)]
+        position_of_mel = np.arrange(1, melspectrogram_data.shape[0] + 1) #position_of_mel=[1,2,...,no. of timestep] mel.shape[0]=num of time steps
         
-    """
-    text_length = len(text)
-    position_of_text = np.arrange(1, text_length + 1) #position_of_text=[1,2,...,text_length(int)]
-    position_of_mel = np.arrange(1, melspectrogram_data.shape[0] + 1) #position_of_mel=[1,2,...,no. of timestep] mel.shape[0]=num of time steps
-    
-    sample = {'text': text, 'mel': melspectrogram_data, 'text_length': text_length, 'mel_input': mel_input, 'position_of_mel': position_of_mel, 'position_of_text': position_of_text }
-    
-    return sample
+        sample = {'text': text, 'mel': melspectrogram_data, 'text_length': text_length, 'mel_input': mel_input, 'position_of_mel': position_of_mel, 'position_of_text': position_of_text }
+        
+        return sample
 
 class PostDatasets(Dataset):
     """ LJSpeech-1.1 dataset."""
