0972d6b185c712d90bdfff90c39f4ae832be1631
Swayam Shah <swayamshah66@gmail.com>
2024-10-12 16:05:07 +0530

    og repo path

--- hyperparams.py
+++ hyperparams.py
@@ -32,7 +32,7 @@ batch_size = 32
 cleaners='english_cleaners'
 
 # General:
-data_path = '/kaggle/input/mel-mag-prepared-data'
+data_path = '/kaggle/input/ljspeech-1-1-with-mel-and-mag-of-each-audio'
 checkpoint_path = '/kaggle/working'
 sample_path = './samples'
 
